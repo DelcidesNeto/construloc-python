767beecc99c3cf17282f3d4726061fc087f8f092
DelcidesNeto <115945133+DelcidesNeto@users.noreply.github.com>
2022-11-27 15:58:46 -0300

    Add files via upload

---
+++ static/script/script.js
@@ -0,0 +1,40 @@
+function mudar_tamanho(){
+    if (innerWidth >= 607){
+        var opcoes = window.document.querySelector('#opcoes')
+        opcoes.style.display = 'none'
+    }
+}
+
+
+function mostrar_menu(){
+    var opcoes = window.document.querySelector('#opcoes')
+    if (opcoes.style.display == 'block'){
+        opcoes.style.display = 'none'
+    }else{
+        opcoes.style.display = 'block'
+    }
+}
+
+
+function mostrar_botao(n){
+    var botao = window.document.querySelector(`#botao${n}`)
+    botao.style.display = 'block'
+}
+
+
+function esconder_botao(n){
+    var botao = window.document.querySelector(`#botao${n}`)
+    botao.style.display = 'none'
+}
+
+
+function mostrar_imagem(n){
+    var img = window.document.querySelector(`#img${n}`)
+    img.style.opacity = '100%'
+}
+
+
+function escurecer_imagem(n){
+    var img = window.document.querySelector(`#img${n}`)
+    img.style.opacity = '75%'
+}
