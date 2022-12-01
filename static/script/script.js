9d8421cd3a0afde751871cbeee4a28df82ac2493
DelcidesNeto <delcides.c.c.neto@gmail.com>
2022-12-01 18:00:21 -0300

    Nova versão
    Novo layout

--- static/script/script.js
+++ static/script/script.js
@@ -4,45 +4,8 @@ function redirecionar(){
         window.location.href = 'https://construloc.herokuapp.com/'
     }
 }
-
-
-function mudar_tamanho(){
-    if (innerWidth >= 607){
-        var opcoes = window.document.querySelector('#opcoes')
-        opcoes.style.display = 'none'
-    }
-}
-
-
-function mostrar_menu(){
-    var opcoes = window.document.querySelector('#opcoes')
-    if (opcoes.style.display == 'block'){
-        opcoes.style.display = 'none'
-    }else{
-        opcoes.style.display = 'block'
-    }
-}
-
-
-function mostrar_botao(n){
-    var botao = window.document.querySelector(`#botao${n}`)
-    botao.style.display = 'block'
-}
-
-
-function esconder_botao(n){
-    var botao = window.document.querySelector(`#botao${n}`)
-    botao.style.display = 'none'
-}
-
-
-function mostrar_imagem(n){
-    var img = window.document.querySelector(`#img${n}`)
-    img.style.opacity = '100%'
-}
-
-
-function escurecer_imagem(n){
-    var img = window.document.querySelector(`#img${n}`)
-    img.style.opacity = '75%'
-}
+window.sr = ScrollReveal({ reset: true })
+sr.reveal('.betoneira', { duration: 1500 })
+sr.reveal('.descricao_betoneira', { duration: 2000 })
+sr.reveal('.alugar', { duration: 2500 })
+sr.reveal('.andaime', { duration: 1500 })
